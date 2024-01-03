473cca1cdf6b40086b8e007bccf29310ad9c0311
remus00 <135813551+remus00@users.noreply.github.com>
2024-01-03 14:46:26 +0100

    Callbacks

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -2,6 +2,9 @@ import { auth, signOut } from '@/auth';
 
 const SettingsPage = async () => {
     const session = await auth();
+
+    session?.user.id;
+
     return (
         <div>
             {JSON.stringify(session)}
