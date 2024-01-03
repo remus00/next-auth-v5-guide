5f5fe3f30daf3bc7f6e9665f8e04466fec2fcc34
remus00 <135813551+remus00@users.noreply.github.com>
2024-01-03 10:48:48 +0100

    Create an user

--- actions/register.ts
+++ actions/register.ts
@@ -1,5 +1,8 @@
 'use server';
+import { getUserByEmail } from '@/data/user';
+import { db } from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
+import bcrypt from 'bcrypt';
 import * as z from 'zod';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -9,5 +12,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         return { error: 'Invalid fields!' };
     }
 
-    return { success: 'Register email sent!' };
+    const { email, password, name } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+        return { error: 'Email already in use' };
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        },
+    });
+
+    /* TODO : Send verification token email */
+
+    return { success: 'User created' };
 };
